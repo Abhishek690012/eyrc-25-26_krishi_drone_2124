f82eed7dfe9fbdc87cc7c8c30d72dc77d2d54a69
Yash Shah <yashyogeshshah@gmail.com>
2025-10-25 17:08:11 +0530

    task 2A creation

---
+++ build/swift_pico_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yash-shah/eyrc-25-26_krishi_drone_2124/swift_pico_description
+# Build directory: /home/yash-shah/eyrc-25-26_krishi_drone_2124/build/swift_pico_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/waypoint_navigation/waypoint_navigation__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yash-shah/eyrc-25-26_krishi_drone_2124/build/waypoint_navigation/waypoint_navigation__py
+# Build directory: /home/yash-shah/eyrc-25-26_krishi_drone_2124/build/waypoint_navigation/waypoint_navigation__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
